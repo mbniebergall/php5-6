a61a57b29dfbde6e045320ef8d783296a112ecc8
mbniebergall <mbniebergall@gmail.com>
2015-05-06 13:05:01 -0600

    corrected gmp_init

--- new/GmpOverload.php
+++ new/GmpOverload.php
@@ -1,9 +1,14 @@
 <?php
 
-$a = 2;
-$b = 4;
-$c = 8;
+$a = gmp_init(2, 10);
+$b = gmp_init(4, 10);
+$c = gmp_init(8, 10);
 
-var_dump($a + $b);
-var_dump($a * $b);
-var_dump(($a ** $b) / $c);
+$add         = $a + $b;
+$multiply    = $a * $b;
+$exponential = ($a ** $b) / $c;
+
+var_dump((int) $add);
+var_dump((int) $multiply);
+var_dump((string) $exponential);
+
